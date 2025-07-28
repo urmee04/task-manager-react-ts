be2961186d238d00c3e1a4c821efad89aeb10714
urmee04 <urmee04sust@gmail.com>
2025-07-28 08:48:46 -0400

    Add TaskList component to render a list of TaskItem components with status change and delete support

---
+++ src/components/TaskList/TaskList.tsx
@@ -0,0 +1,25 @@
+/*TaskList component receives a list of tasks and renders a TaskItem for each.
+It passes down the status change and delete handlers to each TaskItem.*/
+
+import React from "react";
+import { type TaskListProps } from "../../types";
+import { TaskItem } from "../TaskItem/TaskItem";
+
+export const TaskList: React.FC<TaskListProps> = ({
+  tasks,
+  onStatusChange,
+  onDelete,
+}) => {
+  return (
+    <div>
+      {tasks.map((task) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          onStatusChange={onStatusChange}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+};
